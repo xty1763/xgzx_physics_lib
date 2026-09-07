e7ecf50e48c62a4ac23ff3bdc583b3046b6b26a5
xty1763 <1596500035@qq.com>
2026-09-07 15:05:01 +0800

    删除资源：小船过河仿真模拟

--- data/resources.js
+++ data/resources.js
@@ -4,28 +4,6 @@
  */
 window.MANIFEST = [
   {
-    id: "demo-ch1s1-a",
-    title: "第一章示范资源（空页测试）",
-    desc: "点击可进入必修一第一章第一个测试网页（空白页）。",
-    book: "b1",
-    chapter: "b1c1",
-    section: "b1c1s1",
-    url: "pages/ch1-s1.html",
-    tags: ["示范", "测试"],
-    type: "课件"
-  },
-  {
-    id: "r1788620982863p8b6d",
-    title: "小船过河仿真模拟",
-    desc: "",
-    book: "b2",
-    chapter: "b2c1",
-    section: "b2c1s2",
-    url: "pages/res-resource-wjr3u.html",
-    tags: [],
-    type: "仿真资源"
-  },
-  {
     id: "r178876252931582voe",
     title: "波的叠加",
     desc: "",
